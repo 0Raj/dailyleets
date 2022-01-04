3acfe86f0a9d66ece66f1bfcf23a624383909f5d
0Raj <rajasekark0@gmail.com>
2022-01-04 20:28:40 +0530

    .

---
+++ plusOne.js
@@ -0,0 +1,15 @@
+// var digits = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3];
+// var temp_str="";
+//     for(var i=0; i<digits.length; i++) {
+//         temp_str = temp_str+digits[i];
+//     }
+//     console.log(temp_str);
+//     temp_str = Number(temp_str);
+//     console.log(temp_str);
+//     temp_str = temp_str+1;
+//     temp_str = temp_str.toString();
+//     temp_str = temp_str.split("").map(Number);
+//     console.log(temp_str);
+var num = 6145390195186705000;
+num = num+1;
+console.log(num);
